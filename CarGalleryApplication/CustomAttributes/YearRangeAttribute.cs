54b96440ec0781e1cdc27a8854b07dd3de0306d0
ZAFER <ZAFER@LAPTOP-800AO19Q>
2022-08-06 19:54:19 +0300

    Add project files.

---
+++ CarGalleryApplication/CustomAttributes/YearRangeAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarGalleryApplication.CustomAttributes
+{
+    public class YearRangeAttribute : RangeAttribute
+    {
+        public YearRangeAttribute() : base(typeof(int), DateTime.Now.AddYears(-50).Year.ToString(), DateTime.Now.AddYears(1).Year.ToString())
+        {
+
+        }
+    }
+}
---
+++ CarGalleryApplication/Models/Car.cs
@@ -0,0 +1,39 @@
+﻿using CarGalleryApplication.CustomAttributes;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarGalleryApplication.Models
+{
+    public class Car
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement]
+        [Required]
+        public string Brand { get; set; }
+
+        [BsonElement]
+        [Required]
+        public string Model { get; set; }
+
+        [BsonElement("Year")]
+        [Required]
+        [YearRange]
+        public int Year { get; set; }
+
+        [BsonElement("Price")]
+        [Display(Name = "Price($)")]
+        [DisplayFormat(DataFormatString = "{0:#,0}")]
+        public decimal Price { get; set; }
+
+        [BsonElement("ImageUrl")]
+        [Display(Name = "Photo")]
+        [DataType(DataType.ImageUrl)]
+        [Required]
+        public string ImageUrl { get; set; }
+
+    }
+}
